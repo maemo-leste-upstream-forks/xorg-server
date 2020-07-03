c16516903719d5ef4863a4c724c96f76ac91d230
Merlijn Wajer <merlijn@wizzup.org>
2020-07-03 15:36:32 +0200

    WIP: Fix XRecord to handle ButtonPress events from touchscreens
    See:
    
    * https://gitlab.freedesktop.org/xorg/xserver/-/issues/1046
    * https://lists.x.org/archives/xorg-devel/2020-July/058582.html

--- record/record.c
+++ record/record.c
@@ -638,70 +638,6 @@ RecordAReply(CallbackListPtr *pcbl, void *nulldata, void *calldata)
     }                           /* end for each context */
 }                               /* RecordAReply */
 
-/* RecordADeliveredEventOrError
- *
- * Arguments:
- *	pcbl is &EventCallback.
- *	nulldata is NULL.
- *	calldata is a pointer to a EventInfoRec (include/dix.h)
- *	  which provides information about events that are being sent
- *	  to clients.
- *
- * Returns: nothing.
- *
- * Side Effects:
- *	The event or error is recorded by all contexts that have registered
- *	it for this client.
- */
-static void
-RecordADeliveredEventOrError(CallbackListPtr *pcbl, void *nulldata,
-                             void *calldata)
-{
-    EventInfoRec *pei = (EventInfoRec *) calldata;
-    RecordContextPtr pContext;
-    RecordClientsAndProtocolPtr pRCAP;
-    int eci;                    /* enabled context index */
-    ClientPtr pClient = pei->client;
-
-    for (eci = 0; eci < numEnabledContexts; eci++) {
-        pContext = ppAllContexts[eci];
-        pRCAP = RecordFindClientOnContext(pContext, pClient->clientAsMask,
-                                          NULL);
-        if (pRCAP && (pRCAP->pDeliveredEventSet || pRCAP->pErrorSet)) {
-            int ev;             /* event index */
-            xEvent *pev = pei->events;
-
-            for (ev = 0; ev < pei->count; ev++, pev++) {
-                int recordit = 0;
-
-                if (pRCAP->pErrorSet) {
-                    recordit = RecordIsMemberOfSet(pRCAP->pErrorSet,
-                                                   ((xError *) (pev))->
-                                                   errorCode);
-                }
-                else if (pRCAP->pDeliveredEventSet) {
-                    recordit = RecordIsMemberOfSet(pRCAP->pDeliveredEventSet,
-                                                   pev->u.u.type & 0177);
-                }
-                if (recordit) {
-                    xEvent swappedEvent;
-                    xEvent *pEvToRecord = pev;
-
-                    if (pClient->swapped) {
-                        (*EventSwapVector[pev->u.u.type & 0177])
-                            (pev, &swappedEvent);
-                        pEvToRecord = &swappedEvent;
-
-                    }
-                    RecordAProtocolElement(pContext, pClient,
-                                           XRecordFromServer, pEvToRecord,
-                                           SIZEOF(xEvent), 0, 0);
-                }
-            }                   /* end for each event */
-        }                       /* end this client is on this context */
-    }                           /* end for each enabled context */
-}                               /* RecordADeliveredEventOrError */
-
 static void
 RecordSendProtocolEvents(RecordClientsAndProtocolPtr pRCAP,
                          RecordContextPtr pContext, xEvent *pev, int count)
@@ -748,7 +684,81 @@ RecordSendProtocolEvents(RecordClientsAndProtocolPtr pRCAP,
         }
     }                           /* end for each event */
 
-}                               /* RecordADeviceEvent */
+}                               /* RecordSendProtocolEvents */
+
+/* RecordADeliveredEventOrError
+ *
+ * Arguments:
+ *	pcbl is &EventCallback.
+ *	nulldata is NULL.
+ *	calldata is a pointer to a EventInfoRec (include/dix.h)
+ *	  which provides information about events that are being sent
+ *	  to clients.
+ *
+ * Returns: nothing.
+ *
+ * Side Effects:
+ *	The event or error is recorded by all contexts that have registered
+ *	it for this client.
+ */
+static void
+RecordADeliveredEventOrError(CallbackListPtr *pcbl, void *nulldata,
+                             void *calldata)
+{
+    EventInfoRec *pei = (EventInfoRec *) calldata;
+    RecordContextPtr pContext;
+    RecordClientsAndProtocolPtr pRCAP;
+    int eci;                    /* enabled context index */
+    ClientPtr pClient = pei->client;
+
+    for (eci = 0; eci < numEnabledContexts; eci++) {
+        pContext = ppAllContexts[eci];
+        pRCAP = RecordFindClientOnContext(pContext, pClient->clientAsMask,
+                                          NULL);
+        if (pRCAP && (pRCAP->pDeliveredEventSet || pRCAP->pErrorSet || pRCAP->pDeviceEventSet)) {
+            int ev;             /* event index */
+            xEvent *pev = pei->events;
+
+            for (ev = 0; ev < pei->count; ev++, pev++) {
+                int recordit = 0;
+
+                if (pRCAP->pDeviceEventSet && (pev->u.u.type == MotionNotify ||
+                 pev->u.u.type == ButtonPress ||
+                 pev->u.u.type == ButtonRelease ||
+                 pev->u.u.type == KeyPress ||
+                 pev->u.u.type == KeyRelease)) {
+                    RecordSendProtocolEvents(pRCAP, pContext, pev, 1);
+                    continue;
+                }
+
+                if (pRCAP->pErrorSet) {
+                    recordit = RecordIsMemberOfSet(pRCAP->pErrorSet,
+                                                   ((xError *) (pev))->
+                                                   errorCode);
+                }
+                else if (pRCAP->pDeliveredEventSet) {
+                    recordit = RecordIsMemberOfSet(pRCAP->pDeliveredEventSet,
+                                                   pev->u.u.type & 0177);
+                }
+                if (recordit) {
+                    xEvent swappedEvent;
+                    xEvent *pEvToRecord = pev;
+
+                    if (pClient->swapped) {
+                        (*EventSwapVector[pev->u.u.type & 0177])
+                            (pev, &swappedEvent);
+                        pEvToRecord = &swappedEvent;
+
+                    }
+                    RecordAProtocolElement(pContext, pClient,
+                                           XRecordFromServer, pEvToRecord,
+                                           SIZEOF(xEvent), 0, 0);
+                }
+            }                   /* end for each event */
+        }                       /* end this client is on this context */
+    }                           /* end for each enabled context */
+}                               /* RecordADeliveredEventOrError */
+
 
 /* RecordADeviceEvent
  *
